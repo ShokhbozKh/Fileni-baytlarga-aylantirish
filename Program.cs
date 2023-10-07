251c4f0713582f4e38b91e07558d2b78b88ba8a0
Shohboz Xurramov <shohboztuit@gmail.com>
2023-10-07 09:13:06 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // string path = @"D://hello";
+            //// Directory.CreateDirectory(path);
+            // Console.WriteLine(Directory.GetParent(path));
+
+
+
+            // var papka = Directory.EnumerateFileSystemEntries(path);
+            // foreach(var  papkaEntry in papka)
+            // {
+            //     Console.WriteLine(papkaEntry);
+            // }
+
+            //string path = @"D://Hello.txt";
+            //FileInfo fileInfo = new FileInfo(path);
+
+            //if(!fileInfo.Exists)
+            //{
+            //    fileInfo.Create();
+
+            //}
+            //StreamWriter streamWriter = fileInfo.AppendText();
+            //for(int i = 0; i < 12; i++)
+            //{
+            //    streamWriter.WriteLine(i);
+
+            //}
+            //streamWriter.Close();
+
+            string path = @"D://folder";
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            directoryInfo.Create();
+
+            string path2 = @"D:/folder/music.M4A";// file joyi
+            byte[] buffer = File.ReadAllBytes(path2);// fileni baytlarga utkazib yozish
+            Array.Reverse(buffer);//massivni teskari ugirish
+            File.WriteAllBytes(path2, buffer);// qayta yozish
+            Console.WriteLine($"BAytlar soni: {buffer.Length}");
+
+
+
+
+
+        }
+    }
+}
